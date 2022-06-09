550f1dfb942785dcff6be6ac35dfc49ed0463a74
thisisaaronland <aaron@localhost>
2022-06-08 17:20:48 -0700

    Update OpenBucket to be selective about appending a ?metadata=skip parameter

--- bucket/bucket.go
+++ bucket/bucket.go
@@ -17,13 +17,38 @@ func OpenBucket(ctx context.Context, bucket_uri string) (*gc_blob.Bucket, error)
 		return nil, fmt.Errorf("Failed to parse bucket URI, %w", err)
 	}
 
-	q := u.Query()
+	if allowsToSkipMetadata(u) {
 
-	if q.Get("metadata") != "skip" {
-		q.Set("metadata", "skip")
-		u.RawQuery = q.Encode()
-		bucket_uri = u.String()
+		q := u.Query()
+
+		if q.Get("metadata") != "skip" {
+			q.Set("metadata", "skip")
+			u.RawQuery = q.Encode()
+			bucket_uri = u.String()
+		}
 	}
 
 	return gc_blob.OpenBucket(ctx, bucket_uri)
+}
+
+// allowsToSkipMetadata returns a boolean value indicating whether or not the blob opener for
+// 'u' supports the '?metadata=skip' parameter
+func allowsToSkipMetadata(u *url.URL) bool {
+
+	allowed := false
+
+	allowed_schemes := []string{
+		"file",
+	}
+
+	scheme := u.Scheme
+
+	for _, s := range allowed_schemes {
+		if scheme == s {
+			allowed = true
+			break
+		}
+	}
+
+	return allowed
 }
--- bucket/bucket_test.go
+++ bucket/bucket_test.go
@@ -1,10 +1,29 @@
 package bucket
 
 import (
+	"context"
+	_ "gocloud.dev/blob/fileblob"
+	_ "gocloud.dev/blob/memblob"
+	_ "gocloud.dev/blob/s3blob"
 	"testing"
 )
 
 func TestOpenBucket(t *testing.T) {
 
-	t.Skip()
+	uris := []string{
+		"file:///tmp",
+		"mem://",
+		"s3://example?region=us-east-1",
+	}
+
+	for _, uri := range uris {
+
+		ctx := context.Background()
+
+		_, err := OpenBucket(ctx, uri)
+
+		if err != nil {
+			t.Fatalf("Failed to create bucket for '%s', %v", uri, err)
+		}
+	}
 }
